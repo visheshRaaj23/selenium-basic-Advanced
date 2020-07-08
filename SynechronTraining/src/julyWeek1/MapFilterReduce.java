26553114aba6c549c86722509e16866902dfb537
VisheshUdupa23 <49617465+VisheshUdupa23@users.noreply.github.com>
2020-07-08 14:04:00 +0530

    Update MapFilterReduce.java

--- SynechronTraining/src/julyWeek1/MapFilterReduce.java
+++ SynechronTraining/src/julyWeek1/MapFilterReduce.java
@@ -6,20 +6,38 @@ import java.util.stream.Stream;
 public class MapFilterReduce {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		oldStyleOperation();
+		//map_Operation();
+		//filter_Operation();
+		reductionOperation();
 	}
 	
-	public static void oldStyleOperation() {
+	public static void map_Operation() {
 		String[] players = new String[] {"Vishesh","Supriya","Anuraaga","Prajna"};
 		System.out.println(players.length);
 		Stream<String> playerStream = Arrays.stream(players);
 		Stream<String> upperCaseStream = playerStream.map(player -> player.toUpperCase());
 		String[] newPlayers = upperCaseStream.toArray(String[]::new);
-		
 		for(String player:newPlayers) {
 			System.out.println(player);
 		}
+	}
+	public static void filter_Operation() {
+		String[] players = new String[] {"Vishesh","Supriya","Anuraaga","Prajna"};
+		System.out.println(players.length);
+		Stream<String> playerStream = Arrays.stream(players);
+		Stream<String> upperCaseStream = playerStream.filter(player -> player.length()<=7);
+		String[] newPlayers = upperCaseStream.toArray(String[]::new);
+		for(String player:newPlayers) {
+			System.out.println(player);
+		}
+	}
+	
+	public static void reductionOperation() {
+		long[] playersSalary = {230,245,156,456,186,187,190};
+		System.out.println(Arrays.stream(playersSalary).sum());
+		System.out.println(Arrays.stream(playersSalary).min());
+		System.out.println(Arrays.stream(playersSalary).max());
+		System.out.println(Arrays.stream(playersSalary).count());
 	}
 
 }
