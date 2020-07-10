9ef0f233692c7e7da4c14da6c27a9b9b7b204d4d
visheshRaaj23 <49617465+VisheshUdupa23@users.noreply.github.com>
2020-07-10 15:37:46 +0530

    My First Git Commit from Git Bash

---
+++ SynechronTraining/src/julyWeek1/ShallowClone.java
@@ -0,0 +1,51 @@
+package julyWeek1;
+
+import java.util.Arrays;
+
+public class ShallowClone implements Cloneable {
+
+	public int[] numbers;
+	public String nameString = "Supriya";
+	public float value = 5.5f;
+	public Float floatWrapper = 23.9f;
+
+	public ShallowClone(int size) {
+		numbers = new int[size];
+	}
+
+	public void cloneTest(int size) {
+		// THE INT ARRAY IS SHALLOW CLONED AS ONLY THE ADDRESS IS COPIED AND THE VALUES
+		// ARE NOT COPIED
+		ShallowClone originalObject = new ShallowClone(size);
+		originalObject.numbers[0] = 100;
+		try {
+			ShallowClone clonedObject = (ShallowClone) originalObject.clone();
+			System.out.println("Cloned Successfully");
+			System.out.println("Original Object" + originalObject);
+			System.out.println("Cloned Object" + clonedObject);
+			originalObject.numbers[1] = 500;
+			nameString = "Vishesh";
+			value = 8.8f;
+			floatWrapper = 23.9f;
+			System.out.println("Original Object" + originalObject);
+			System.out.println("Cloned Object" + clonedObject);
+
+		} catch (CloneNotSupportedException exception) {
+			System.out.println("You cannot clone this Object");
+			exception.printStackTrace();
+		}
+
+	}
+
+	/*public static void main(String[] args) {
+		ShallowClone clone = new ShallowClone(2);
+		clone.cloneTest(2);
+	}*/
+
+	@Override
+	public String toString() {
+		return "ObjectToBeCloned[numbers:" + Arrays.toString(numbers) + ",nameString:[" + nameString + "],value:["
+				+ floatWrapper + "],floatWrapper[" + floatWrapper + "]";
+	}
+
+}
---
+++ SynechronTraining/src/julyWeek1/ShallowDriver.java
@@ -0,0 +1,14 @@
+package julyWeek1;
+
+public class ShallowDriver extends ShallowClone {
+	public ShallowDriver(int size) {
+		super(size);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		ShallowClone driver = new ShallowClone(2);
+		driver.cloneTest(2);
+	}
+
+}
